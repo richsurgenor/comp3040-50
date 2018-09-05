bdd9703d6ad9d21920c58c07a8ea699b615cad1b
root <root@DESKTOP-04Q3FDV.localdomain>
2018-09-05 14:59:09 -0500

    deleted unnecessary files

--- Objects/startup_stm32l100xc.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_stm32l100xc.o: RTE\Device\STM32L100RC\startup_stm32l100xc.s
--- RTE/_Target_1/RTE_Components.h
+++ RTE/_Target_1/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'lab1' 
+ * Project: 'lab2' 
  * Target:  'Target 1' 
  */
 
