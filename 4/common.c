0c6b338933c9bc5fef78232ac021c313e678061f
Rich Surgenor <krs0048@auburn.edu>
2018-09-14 09:37:31 -0500

    Update common.c

--- 4/common.c
+++ 4/common.c
@@ -6,7 +6,8 @@ Common variables/functions throughout the program
 #include "common.h"
 
 /* delay in ms */
-void delay (int ms) {
+void delay (int ms) 
+{
 	int i,j,n;
 	for (j=0; j<266*ms; j++) { 
 		n = j; //dummy operation for single-step test
