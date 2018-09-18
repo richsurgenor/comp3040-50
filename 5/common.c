9c635fb6062c30dd2ee06012921aadc831ccc4e4
Rich Surgenor <krs0048@auburn.edu>
2018-09-18 00:31:04 -0500

    finished lab5

---
+++ 5/common.c
@@ -0,0 +1,61 @@
+/*
+common.c
+Common variables/functions throughout the program
+*/
+
+#include "common.h"
+#include "STM32L1xx.h"
+
+volatile counter counters[] = { // TODO: Move this to main()
+	{ .count = 0, .direction = COUNTING_UP, .speed = 1000 },
+	{ .count = 0, .direction = COUNTING_UP, .speed = 500}
+};
+
+/* delay in ms */
+void delay (int ms)
+{
+	int j,n;
+	for (j=0; j<266*ms; j++) {
+		n = j; //dummy operation for single-step test
+	} //do nothing
+}
+
+void update_counters(void)
+{
+	uint16_t output = 0;
+	output |= (GPIOC->ODR & 0x0300); // keep status LEDs' states
+
+	for (int i = 0; i < COUNTERS_SIZE; i++) {
+		output |= counters[i].count << i*4;
+	}
+
+	GPIOC->ODR = output;
+}
+
+/* Initialize GPIO pins used in the program
+ * PC3-0 = virtual LEDs
+ * PC8 = blue LED, PC9 = green LED */
+void enable_counter_GPIO(void)
+{
+	/* Configure PC3-PC0 as output pins to drive virtual LEDs */
+	RCC->AHBENR |= 0x04;           // Enable GPIOC clock (bit 2)
+	GPIOC->MODER &= ~(0x0000FFFF); // Clear PC7-PC0 mode bits
+	GPIOC->MODER |= (0x00005555);  // General purpose output mode
+	GPIOC->BSRR = 0x00FF << 16;    // Reset PC7-PC0 output bits to 0
+
+	/* Configure PC8,PC9 as output pins to drive LEDs */
+	GPIOC->MODER &= ~(0x000F0000); // Clear PC9-PC8 mode bits
+	GPIOC->MODER |= (0x00050000);  // General purpose output mode
+	GPIOC->BSRR = 0x0300 << 16;    // Reset PC9-PC8 output bits to 0
+}
+
+/* counter function - decade up counter between 0 & 9
+ * Green LED = counting up, Blue LED = counting down */
+void counting0(void)
+{
+	counters[0].count++;
+	// Do not allow counter to leave decided range
+	if ( counters[0].count > COUNTER_HIGH ) {
+		counters[0].count = 0;
+	}
+}
---
+++ 5/interrupt_handler.c
@@ -0,0 +1,75 @@
+
+#include "interrupt_handler.h"
+#include "keypad.h"
+#include "common.h"
+
+uint8_t display_keypad_count = 0;
+
+/* Process of interrupt signal on our board:
+1. Interrupt signal comes in on GPIO pin
+2. Edge detection
+3. Software interrupt event register => IMR
+4. Interrupt mask register => PR
+
+5. IRQ TO NVIC
+
+All paths must be cleared for signal to be processed correctly.
+*/
+
+/*For lab 4 specifically: Two GPIO pins, PA0 and PA1, are to be configured as external
+interrupt signals EXTI0 and EXTI1*/
+
+/* Unblocks CPU signals from PA0 and PA1 specifically, and then
+   does this in the NVIC as well.
+ */
+// TODO: make more generic
+void init_interrupts(volatile uint32_t *SYSCFG_EXTICR)
+{
+	//SystemInit();
+	//RCC_APB2ENR_SYSCFGEN
+	__enable_irq();
+
+	/*
+	0000: PA[x] pinX
+	0001: PB[x] pin
+	0010: PC[x] pin
+	0011: PD[x] pin
+	0100: PE[x] pin
+	*/
+	*SYSCFG_EXTICR &= ~(SYSCFG_EXTICR1_EXTI1); // 0x000000F0
+
+	//EXTI0 and EXTI1 are both rising edge triggered
+	EXTI->FTSR |= EXTI_FTSR_TR1;
+
+	//EXTI0 and EXT1 lines are both unmasked
+	EXTI->IMR |= EXTI_IMR_MR1;
+
+	//Enable NVIC IRQ lines for
+	NVIC_EnableIRQ(EXTI1_IRQn);
+
+	NVIC_SetPriority(EXTI1_IRQn, 1);
+}
+
+/* EXTI1 Interrupt Handler
+ * Switches direction of counter[1] to count up, toggles green LED
+ */
+void EXTI1_IRQHandler(void)
+{
+	uint16_t loops = 50;
+	for (int k = 0; k < loops; k++);
+
+	bool failure = false;
+	location loc = detect(&failure);
+
+	if (!failure) {
+		GPIOC->ODR = keys[loc.row][loc.col] | ( GPIOC->ODR & GREEN_LED );
+		display_keypad_count = SECONDS_TO_DISPLAY_KEYPAD_PRESS;
+	} else {
+		GPIOC->ODR |= BLUE_LED;
+	}
+
+	counters[1].direction = COUNTING_UP;
+
+	GPIOB->ODR &= ~(0xF0); // Clear PB7-4
+	EXTI->PR |= EXTI_PR_PR1;
+}
